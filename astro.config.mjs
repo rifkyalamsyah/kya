21705f4b16c668a7d1c32cc2433d2c173b4075e9
Rifky Alamsyah <rifkyalamsyah30@gmail.com>
2025-03-28 00:53:32 +0700

    feat: view transitions & fix content collection

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [mdx()],
+  integrations: [mdx(), icon({iconDir: "src/assets/icons"})],
   experimental: {
     svg: {
       mode: 'sprite',
