3f3770c1294ef3c88fcc7c2f4a811ee8fc6962e2
Rifky Alamsyah <rifkyalamsyah30@gmail.com>
2025-06-10 11:26:36 +0700

    upgrade astro v5.9.2

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
-import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [mdx(), icon({iconDir: "src/assets/icons"})],
-  experimental: {
-    svg: {
-      mode: 'sprite',
-    },
-  },
+  integrations: [mdx()],
 });
