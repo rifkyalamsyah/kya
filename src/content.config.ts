baef8731d8d33469ab6785935670049ba9ed6285
Rifky Alamsyah <rifkyalamsyah30@gmail.com>
2025-07-15 22:59:17 +0700

    fix: container

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
 const projects = defineCollection({
-  // Load Markdown and MDX files in the `src/content/blog/` directory.
+  // Load Markdown and MDX files in the `src/projects/` directory.
   loader: glob({ base: './src/projects', pattern: '**/*.{md,mdx}' }),
   // Type-check frontmatter using a schema
   schema: ({ image }) => z.object({
