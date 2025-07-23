529d52734aa6d57040752d14195271d1a9d1fff5
Rifky Alamsyah <rifkyalamsyah30@gmail.com>
2025-07-23 13:52:26 +0700

    fix: images

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,18 +6,34 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://kya.pages.dev",
+  site: 'https://kya.pages.dev',
   vite: {
     plugins: [tailwindcss()],
   },
   integrations: [mdx()],
+  image: {
+    responsiveStyles: true,
+    layout: 'constrained',
+  },
   experimental: {
-        fonts: [{
-            provider: fontProviders.google(),
-            name: "Outfit",
-            cssVariable: "--font-outfit",
-            weights: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-            styles: ["normal"],
-        }]
-    }
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: 'Outfit',
+        cssVariable: '--font-outfit',
+        weights: [
+          '100',
+          '200',
+          '300',
+          '400',
+          '500',
+          '600',
+          '700',
+          '800',
+          '900',
+        ],
+        styles: ['normal'],
+      },
+    ],
+  },
 });
